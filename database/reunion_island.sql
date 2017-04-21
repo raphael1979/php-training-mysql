17495bd0814c8dad8ea3db303d0026fa6c8302a9
raphael <moudens.raphael@orange.fr>
2017-04-21 16:59:01 +0200

    exo rando en cours

--- database/reunion_island.sql
+++ database/reunion_island.sql
@@ -24,11 +24,19 @@ USE `reunion_island`;
 
 DROP TABLE IF EXISTS `hiking`;
 CREATE TABLE IF NOT EXISTS `hiking` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `difficulty` enum('très facile','facile','moyen','difficile','très difficile') NOT NULL,
-  `distance` int(11) NOT NULL COMMENT 'in km',
-  `duration` time NOT NULL,
-  `height_difference` int(6) NOT NULL COMMENT 'in m',
-  PRIMARY KEY (`id`)
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`difficulty` enum('très facile','facile','moyen','difficile','très difficile') NOT NULL,
+	`distance` int(11) NOT NULL COMMENT 'in km',
+	`duration` time NOT NULL,
+	`height_difference` int(6) NOT NULL COMMENT 'in m',
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+INSERT INTO `hiking`(`name`,`difficulty`,`distance`,`duration`,`height_difference`)
+VALUES ('Le sommet du Piton Béthoune par le tour du bonnet de Prêtre', 'très difficile', 5.7, '04:00:00', 650),
+('Le sentier des Sources entre Cilaos et Bras sec', 'facile', 5.9, '01:15:00', 300),
+('De Bras Sec au Bras de Cilaos par Palmiste Rouge', 'moyen', 16.5, '05:30:00', 1000),
+('La grande cascade du Bras Rouge depuis le pont sur la route RD242', 'moyen', 1.5, '01:30:00', 240),
+('L\'îlet Sopurces et la Ravine Bananes depuis l\'îlet à Cordes', 'difficile', 6.4, '05:00:00', 450);
--- read.php
+++ read.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Randonnées</title>
-    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
-  </head>
-  <body>
-    <h1>Liste des randonnées</h1>
-    <table>
-      <!-- Afficher la liste des randonnées -->
-    </table>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Randonnées</title>
+  <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+  <?php 
+  try{
+    $dbh = new PDO('mysql:host=localhost;dbname=reunion_island','root', 'root');
+    // $dbh = null;
+  }catch(PDOException $e){
+    print"Error !:".$e->getMessage()."<br/>";
+    die();
+  }
+  ?>
+  <h1>Liste des randonnées</h1>
+  <!-- Afficher la liste des randonnées -->
+
+  <table>
+
+    <tr>
+      <th>name</th>
+      <th>difficulty</th>
+      <th>distance</th>
+      <th>duration</th>
+      <th>height_difference</th>
+    </tr>
+    <?php
+    foreach ($dbh->query('SELECT * from hiking') as $row){
+      echo '<tr>.$row["name"]." ".$row["distance"]."</tr> ' ;
+    }
+    ?>
+  </table>
+</body>
 </html>
